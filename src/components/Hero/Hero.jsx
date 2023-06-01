4e052eff5dc020c14689177b9815eb757f6d8b56
Nikolay <sweeterthanfurby@mail. git config --global user.email sweeterthanfurby@mail>
2023-06-01 17:12:56 +0300

    fill:animations

--- src/components/Hero/Hero.jsx
+++ src/components/Hero/Hero.jsx
@@ -3,8 +3,16 @@ import css from './Hero.module.css'
 import HeroImg from '../../assets/hero.png'
 import {RiShoppingBagLine} from "react-icons/ri";
 import {BsArrowRight} from "react-icons/bs";
+import {motion} from 'framer-motion'
 
 export const Hero = () => {
+
+    const transition = {
+        duration: 3,
+        type: 'spring',
+    }
+
+
     return (
         <div className={css.container}>
 
@@ -24,9 +32,29 @@ export const Hero = () => {
             {/*middle slide*/}
 
             <div className={css.wrapper}>
-                <div className={css.blueCircle}></div>
-                <img src={HeroImg} alt="" width={600}/>
-                <div className={css.cart2}>
+
+                {/*blue*/}
+                <motion.div
+                    initial={{bottom: '2rem'}}
+                    whileInView={{bottom: '0rem'}}
+                    className={css.blueCircle}
+                    transition={transition}
+                ></motion.div>
+
+                {/*hero*/}
+                <motion.img
+                    transition={transition}
+                    initial={{bottom: '-2rem'}}
+                    whileInView={{bottom: '0rem'}}
+                    src={HeroImg} alt="hero" width={600}/>
+
+                {/*cart*/}
+
+                <motion.div
+                    transition={transition}
+                    initial={{right: '4%'}}
+                    whileInView={{right: '2%'}}
+                    className={css.cart2}>
                     <RiShoppingBagLine/>
                     <div className={css.signup}>
                         <span>Best Signup Offers</span>
@@ -34,7 +62,7 @@ export const Hero = () => {
                             <BsArrowRight/>
                         </div>
                     </div>
-                </div>
+                </motion.div>
             </div>
 
             {/*right side*/}
